50675aaf1a87dd9c5ecfc1408717b4c27db68573
Auto Deploy <deploy@botlode.com>
2026-01-15 18:57:44 -0300

    Auto Deploy Web: 2026-01-15 18:57

--- deploy.ps1
+++ deploy.ps1
@@ -5,27 +5,28 @@ Write-Host "1. Limpiando motores..."
 flutter clean
 flutter pub get
 
-# 2. BUILD (¡ESTA ES LA LÍNEA CLAVE!)
-Write-Host "2. Compilando nucleo web (Canvaskit)..."
-flutter build web --release 
+# 2. BUILD
+Write-Host "2. Compilando nucleo web..."
+flutter build web --release
 
-# 3. GITHUB AUTOMATICO
+# 3. GITHUB AUTOMATICO (Sin preguntas)
 $fecha = Get-Date -Format "yyyy-MM-dd HH:mm"
 $commitMsg = "Auto Deploy Web: $fecha"
 
 Write-Host "3. Guardando en GitHub ($commitMsg)..."
 git add .
-git commit -m "$commitMsg" *>$null 
+# El comando git commit puede fallar si no hay cambios, lo silenciamos con '2>$null' para que no pare el script
+git commit -m "$commitMsg" 2>$null 
 git push
 
 # 4. DESPLIEGUE A VERCEL
 Write-Host "4. Subiendo a la Nube (Vercel)..."
 cd build\web
 
-# PRECAUCIÓN: Si crees que estás en el proyecto incorrecto, 
-# Vercel usará la configuración guardada en la carpeta .vercel oculta aquí.
+# --prod: Producción
+# --yes: Sin preguntas
 vercel --prod --yes 
 
-cd .
+cd ..\..
 
 Write-Host "--- DESPLIEGUE COMPLETADO CON EXITO ---" -ForegroundColor Green
