e2b80d27c6906703241e968c2ed7a4e53720a6ad
Auto Deploy <deploy@botlode.com>
2026-01-26 00:48:33 -0300

    vercel

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,14 +1,15 @@
 // Archivo: lib/core/router/app_router.dart
-import 'package:botlode_player/features/player/presentation/widgets/floating_bot_widget.dart';
+// import 'package:botlode_player/features/player/presentation/widgets/floating_bot_widget.dart';
+import 'package:botlode_player/features/player/presentation/widgets/ultra_simple_bot.dart'; // ⬅️ TEST
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    // ÚNICA RUTA: EL BOT FLOTANTE
+    // ÚNICA RUTA: EL BOT ULTRA SIMPLE
     GoRoute(
       path: '/',
-      builder: (context, state) => const FloatingBotWidget(),
+      builder: (context, state) => const UltraSimpleBot(), // ⬅️ TEST
     ),
   ],
 );
